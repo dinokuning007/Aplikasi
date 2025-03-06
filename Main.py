6589e030e45fdba2a15e81dc458fa29d7421dbff
Laptop YAMA <mailfortrash974@gmail.com>
2025-03-07 02:26:10 +0700

    Test Laptop

--- Main.py
+++ Main.py
@@ -8,3 +8,5 @@ if __name__ == '__main__' :
     print('Laporan Gempa Terkini')
     result = Deteksi_Gempa.ekstraksi_data()
     Deteksi_Gempa.tampilkan_data(result)
+
+# EDITNYA KEBACA DI GIT NGGA?
